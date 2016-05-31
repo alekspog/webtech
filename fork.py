e52a850d78c215f0953aee191b829d678ae7a4c5
alekspog <aleksey.pogibelev@gmail.com>
2016-06-01 00:58:31 +0300

    Update fork.py
    fix pid checking

--- fork.py
+++ fork.py
@@ -5,14 +5,15 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('0.0.0.0', 2222))
 sock.listen(10)
 pid = os.fork()
-if pid:
-     conn.close()
-else: # process child -> while True recv sendall etc. conn.close()
 
      while True:
           conn, addr = sock.accept()
-          while True:
-               data = conn.recv(1024)
-               if not data: break
-               conn.send(data)
-          conn.close()
+          if pid:
+               conn.close()
+          else:
+               
+               while True:
+                    data = conn.recv(1024)
+                    if not data: break
+                    conn.send(data)
+               conn.close()
